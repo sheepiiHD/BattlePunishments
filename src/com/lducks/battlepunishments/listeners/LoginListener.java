11140ce6778ea30574acd166c739ccd9b72a6a8f
lDucks <lducksusa@gmail.com>
2013-07-04 12:01:27 -0400

    Tries to fix 3

--- src/com/lducks/battlepunishments/listeners/LoginListener.java
+++ src/com/lducks/battlepunishments/listeners/LoginListener.java
@@ -6,6 +6,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
@@ -33,7 +34,7 @@ public class LoginListener implements Listener{
 			event.setJoinMessage(null);
 	}
 
-	@EventHandler
+	@EventHandler(priority=EventPriority.HIGHEST)
 	public void onPlayerLogin(PlayerLoginEvent event){
 		Player p = event.getPlayer();
 		String ip = event.getAddress().toString();
